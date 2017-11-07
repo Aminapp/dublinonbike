c0ec6a165df245abf49668bc4a9aa5c178a0f9db
Cris Cris <cristian.rusu@ncirl.ie>
2017-11-07 16:55:56 +0000

    Golfstore application

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -14,14 +13,14 @@
 ActiveRecord::Schema.define(version: 20170301104159) do
 
   create_table "items", force: :cascade do |t|
-    t.string   "title"
-    t.text     "description"
-    t.decimal  "price"
-    t.string   "image_url"
-    t.string   "category"
-    t.string   "brand"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string "title"
+    t.text "description"
+    t.decimal "price"
+    t.string "image_url"
+    t.string "category"
+    t.string "brand"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
